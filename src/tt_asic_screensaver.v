35e772f90834b1be9d507a280f3a825091f2510b
Ethan Sifferman <ethan@sifferman.dev>
2023-08-22 11:56:53 -0700

    connected fpga screensaver

--- src/tt_asic_screensaver.v
+++ src/tt_asic_screensaver.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_asic_screensaver (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -11,50 +11,20 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
+wire clk_25_175 = clk;
+wire rst = !rst_n;
 
-    // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
+wire hsync, vsync;
+wire [3:0] r, g, b;
 
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
+assign {uo_out, uio_out} = {hsync, vsync, r, g, b};
 
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
+assign uio_oe = -1;
 
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    always @(posedge clk) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+top #(.IMAGE_SELECT(2)) screensaver (
+    .clk_25_175(clk_25_175), .rst(rst),
+    .hsync(hsync), .vsync(vsync),
+    .r(r), .g(g), .b(b)
+);
 
 endmodule
